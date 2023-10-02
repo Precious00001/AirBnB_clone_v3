66683ccf87a1d76db32391d6271ccc4480494058
root <lanremidelola@gmail.com>
2023-10-02 03:59:35 +0000

    alx

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
-'''Contains the blueprint for the API.'''
-from flask import Blueprint
+"""Index"""
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from flask import jsonify
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+@app_views.route('/status')
+def get_status():
+    '''Gets the status of the API.
+    '''
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def stats():
+    """
+        return dict count of data
+    """
+    my_dict = {"amenities": storage.count(Amenity),
+               "cities": storage.count(City),
+               "places": storage.count(Place),
+               "reviews": storage.count(Review),
+               "states": storage.count(State),
+               "users": storage.count(User)}
+    return jsonify(my_dict)
