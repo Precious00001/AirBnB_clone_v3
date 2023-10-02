07ef3df5cd5900da2a4a3dadf3b0632b9692ba3a
bossclement <bossclement@gmail.com>
2023-10-02 23:47:56 +0200

    New commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,12 @@ from api.v1.views import app_views
 from flask import Flask
 from os import getenv
 from flask import jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "http://0.0.0.0"}})
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
