7ca2fdeff97c5482308a45d276fa9ef3fa3f95f5
bossclement <bossclement@gmail.com>
2023-10-02 23:33:43 +0200

    New commit

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """users.py"""
-from flask import jsonify, request, abort
+from flask import Flask, jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
+from models.place import Place
 from models.city import City
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
